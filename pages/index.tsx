dbfa0b5effcb40095fad462ff5dd15f32c30e074
vigneshuthra <vigneshuthra@gmail.com>
2022-01-18 02:24:17 +0000

    inital commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        Summa va
+      </div>
+    </>
+  )
+}
+
+export default Home
